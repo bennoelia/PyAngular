75987098bfc08cb3ac568c565a0d246ceff90a38
bennoelia <bennoelia@gmail.com>
2021-11-13 13:38:48 -0300

    Primer Commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  formularioCreado:FormGroup;
+  constructor(private formBuilder:FormBuilder) { 
+   
+  }
+
+  ngOnInit(): void {
+    this.crearFormulario();
+  }
+
+  crearFormulario()
+  {
+    this.formularioCreado=this.formBuilder.group({
+      usuario:['x',Validators.required],
+      pass:['',Validators.compose([
+        Validators.required,Validators.minLength(8)
+      ])
+    ]
+    })
+  }
+
+}
